f38e12650df419a247cc250929c2e9e3582ee922
Daniel Jones <vvsdan999@gmail.com>
2023-10-30 14:24:27 -0400

    add completed project from local

---
+++ src/Context.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [pageId, setPageId] = useState(null);
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        setIsSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        pageId,
+        setPageId,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
---
+++ src/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content. <br /> Anywhere.
+        </h1>
+        <p>
+          Strapi is the loading open-source headless CMS. It's 100% JavaScript
+          and fully customizable.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Hero;
